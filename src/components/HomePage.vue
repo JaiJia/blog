99309579e16008422c4308e541af6afaad5d3882
Jaco <baojiawei1995@gmail.com>
2019-11-07 22:51:41 +0800

    2019-11-07 #Task #Init blog site

---
+++ src/components/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+    <a-layout id="components-layout-demo-top" class="layout">
+        <a-layout-header>
+            <div class="logo">
+                <img src="../assets/logo.png" alt="Logo" width="30">
+            </div>
+            <a-menu
+                    theme="dark"
+                    mode="horizontal"
+                    :style="{ lineHeight: '64px' }"
+            >
+                <a-menu-item key="1">Transition</a-menu-item>
+            </a-menu>
+        </a-layout-header>
+        <a-layout-content style="padding: 0 50px">
+            <a-breadcrumb style="margin: 16px 0">
+                <a-breadcrumb-item>Transition</a-breadcrumb-item>
+            </a-breadcrumb>
+            <div :style="{ background: '#fff', padding: '24px', minHeight: '70vh' }">This is my blog site.</div>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+            Ant Design ©2019
+        </a-layout-footer>
+    </a-layout>
+</template>
+<style>
+    #components-layout-demo-top .logo {
+        width: 120px;
+        height: 31px;
+        float: left;
+    }
+</style>
+
+<script>
+    export default {
+        name: "HomePage"
+    }
+</script>
+
+<style scoped>
+
+</style>
