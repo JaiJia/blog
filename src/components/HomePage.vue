6cf7a2768d708cffa69e6e602260accf2e421727
Jaco <baojiawei1995@gmail.com>
2019-11-08 23:06:37 +0800

    2019-11-08 #Task #Enhance style

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,38 +1,37 @@
 <template>
-    <a-layout id="components-layout-demo-top" class="layout">
-        <a-layout-header>
+    <a-layout id="home-page" class="layout home-page">
+        <a-layout-header class="header">
             <div class="logo">
                 <img src="../assets/logo.png" alt="Logo" width="30">
             </div>
-            <a-menu
-                    theme="dark"
-                    mode="horizontal"
-                    :style="{ lineHeight: '64px' }"
-            >
-                <a-menu-item key="1">Transition</a-menu-item>
+            <a-menu theme="dark" mode="horizontal" style="line-height: 64px;">
+                <a-menu-item key="1">Home</a-menu-item>
             </a-menu>
         </a-layout-header>
-        <a-layout-content style="padding: 0 50px">
-            <a-breadcrumb style="margin: 16px 0">
-                <a-breadcrumb-item>Transition</a-breadcrumb-item>
-            </a-breadcrumb>
-            <div v-for="blog in blogList" :key="blog.id"
-                 :style="{ background: '#fff', padding: '24px', minHeight: '70vh' }">
-                <markdown-it-vue class="md-body" :content="blog.body"/>
-            </div>
+        <aside class="aside">
+            <img src="../assets/logo.png" alt="Profile Photo" width="120">
+            <h3>Jaco</h3>
+            <p>NM$L 🍻</p>
+        </aside>
+        <a-layout-content class="content-wrap">
+            <section class="blog-list">
+                <div v-for="blog in blogList" :key="blog.id">
+                    <h2 class="blog-title">{{blog.title}}</h2>
+                    <markdown-it-vue class="md-body blog-body" :content="blog.abstract"/>
+                    <div class="blog-info">
+                        <span class="blog-info__time">{{blog.created_at | sliceStr(10)}}</span>
+                        <a class="blog-info__label"
+                           :style="{ backgroundColor: '#' + label.color}"
+                              v-for="label in blog.labels" :key="label.id">{{label.name}}</a>
+                    </div>
+                </div>
+            </section>
         </a-layout-content>
         <a-layout-footer style="text-align: center">
             Ant Design ©2019
         </a-layout-footer>
     </a-layout>
 </template>
-<style>
-    #components-layout-demo-top .logo {
-        width: 120px;
-        height: 31px;
-        float: left;
-    }
-</style>
 
 <script>
     import MarkdownItVue from 'markdown-it-vue';
@@ -49,13 +48,93 @@
         },
         created() {
             this.axios.get('https://api.github.com/repos/jaijia/blog/issues').then(rep => {
-                this.blogList = rep.data;
+                this.blogList = rep.data.map(blogItem => {
+                    const blogAbstract = blogItem.body.split('\n').filter(item => {
+                        return item !== '';
+                    }).slice(0, 10);
+                    return Object.assign({
+                        abstract: blogAbstract.join('\n'),
+                    }, blogItem);
+                });
             });
+        },
+        filters: {
+            sliceStr(str, len) {
+                return str.slice(0, len);
+            }
         },
         methods: {}
     }
 </script>
 
-<style scoped>
+<style>
+    .header {
+        display: flex;
+        align-items: center;
+    }
+
+    .logo {
+        margin: 5vw;
+    }
+
+    .aside {
+        position: absolute;
+        left: 100px;
+        top: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .content-wrap {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    .blog-list {
+        min-height: 80vh;
+        padding: 20px;
+        background-color: #fff;
+    }
+
+    .blog-title {
+        font-size: 24px;
+        line-height: 2;
+    }
+
+    .blog-body {
+        padding: 10px;
+        background-color: #f5f5f5;
+    }
+
+    .blog-info {
+        height: 40px;
+        line-height: 40px;
+    }
+
+    .blog-info__time {
+        color: #666;
+    }
+
+    .blog-info__label {
+        display: inline-block;
+        padding: 0 8px;
+        margin: 0 10px;
+        height: 20px;
+        line-height: 20px;
+        color: #000;
+        font-weight: bold;
+    }
+
+    @media only screen and (max-width: 500px) {
+        .aside {
+            display: none;
+        }
+
+        .content-wrap {
+            padding: 0;
+        }
+    }
 
 </style>
